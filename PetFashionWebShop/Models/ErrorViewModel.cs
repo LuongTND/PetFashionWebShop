41796a47f011cc63220ab18ca7950e9790f9b0ba
LuongTND <ducluongtran07@gmail.com>
2024-05-21 01:13:13 +0700

    Add project files.

---
+++ PetFashionWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetFashionWebShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
